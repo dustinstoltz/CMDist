64e0e49217393608789cf13cc0f604ae1c783955
Dustin Stoltz <6200517+dustinstoltz@users.noreply.github.com>
2019-08-21 12:56:43 -0500

    Create parCMDist-helper.R

---
+++ R/parCMDist-helper.R
@@ -0,0 +1,20 @@
+# Private helper function to parallize the computation of distance-matrices
+      .parDist2 <- function(dtm, pd, wem, ind, method) { 
+              # prepare progress bar
+              pb <- utils::txtProgressBar(max = nrow(ind), style = 3)   
+              progress <- function(n) utils::setTxtProgressBar(pb, n)
+              opts <- list(progress = progress)
+            # Compute distance in parallel threads
+            dist <- foreach(
+                      i = 1:nrow(ind), .packages = c("text2vec"), 
+                      .combine = "rbind", .export = ".parDist2", 
+                      .inorder = TRUE, .verbose=FALSE, .options.snow = opts
+                      ) %dopar% {
+                  a <- as.numeric(ind[i,1])
+                  b <- as.numeric(ind[i,2])
+                  dist <- text2vec::dist2(dtm[a:b,], pd, method = RWMD$new(wem, method), norm = 'none')
+                  }
+              # Clean-up
+              close(pb)
+              return(dist)
+          }
